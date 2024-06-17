45882bfd2e1e4ed035d80aca791e2009c1749761
RichardGPCPRO <101729759+RichardGPCPRO@users.noreply.github.com>
2024-06-17 03:56:59 -0300

    Update CompletarIncompleto3TrabalhoKleber.cpp

--- CompletarIncompleto3TrabalhoKleber.cpp
+++ CompletarIncompleto3TrabalhoKleber.cpp
@@ -1,53 +1,150 @@
 #include<stdio.h>
 
-#include<locale.h>
+#include<stdlib.h>
 
-struct cliente{
+struct produto{
 
-int cpf;
-char nascimento[50]; };
+int codigo;
 
-struct cliente clientes [200];
+char descricao[50];
 
-int codigo=0;
+double preco;
 
-void vender Ingresso(){
+int estoque;
+
+};
+
+struct produto p[200];
+
+void menu(){
 
 system("cls");
 
-fflush(stdin);
+printf("\nDigite a opção desejada"); printf("\n1-Cadastro de produtos");
 
-gets(clientes[codigo].nome);
+printf("\n3-Vendas realizadas");
 
-printf("\nDigite o CPF: ");
+printf("\n4-Gerenciamento de estoque");
 
-scanf("%i",&clientes [codigo].cpf);
+printf("\n5-Sair");
 
-printf("\nDigite a data de nascimento "),
-
-fflush(stdin);
-
-gets(clientes [codigo] nascimento);
-
-printf("\nTegresso vendidoal\n"), system("pause);
+printf("\nOpção: ");
 
 }
 
-void listarIngresso(){
+int seq=0, vd=0;
+
+double vendas [200];
+
+void cadastro(){
 
 system("cls");
 
+p[seq].codigo-seq+1;
+
+printf("\nDigite o nome do produto: ");
+
+fflush(stdin);
+
+gets(p[seq].descricao);
+
+printf("\nDigite o preço do produto: ");
+
+scanf("%lf",&p[seq].preco);
+
+printf("\nDigite o estoque inicial: ");
+
+seq++;
+
+}
+
+void venda(){
+
+char resp;
+
+int cod, cont, qtde, existe=1;
+
+double valorTotal=0;
+
+do{
+
+printf("\nDigite o código do produto: ");
+
+scanf("%i",&cod);
+
+for(cont=0;cont<seq;cont++) {
+
+if(cod==p[cont].codigo){
+
+printf("\nProduto:
+
+%s\n",p[cont].descricao);
+
+printf("\nEstoque:
+
+%i",p[cont].estoque);
+
+printf("\nDigite a quantidade: ");
+
+scanf("%i",&qtde);
+
+p[cont].estoque-=qtde;
+
+valorTotal=(qtde*p[cont].preco)+val
+
+orTotal;
+
+existe=1;
+
+//valorTotal+=(qtde*p[cont].preco);
+
+printf("\ndeseja mais algum
+
+produto? s-sim n-não: ");
+
+fflush(stdin);
+
+scanf("%c",&resp);
+
+}else{
+
+existe=0;
+
+}
+
+}
+
+printf("\nProduto não cadastrado!\n");
+
+system("pause");
+
+}
+
+}while(resp!='n');
+
+printf("\no valor total da venda é: %.21f
+
+\n", valor Total);
+
+vendas [vd]=valorTotal;
+
+vd++;
+
+} void relatorio(){
+
 int x;
 
-printf("\n******************\n");
+for(x=0;x<vd; x++) {
 
-printf("\nNome: %s", clientes[x].nome);
+printf("\n*********************\n");
 
-printf("\nCPF: %i", clientes[x].cpf);
+printf("\nVenda n: %i valor:
 
-printf("\nData Nascimento
+21f\n",x+1, vendas [x]);
 
-%s\n", clientes [x].nascimento);
+//printf("Estoque atual:
+
+%1\n",p[x].estoque);
 
 }
 
@@ -55,8 +152,78 @@ system("pause");
 
 }
 
-void validarIngresso(){
+void gerenciamento(){
 
-system("cls");
+int y, codp,qtde;
 
+scanf("%i",&codp);
 
+for(y=0;y<seq;y++){
+
+if(codp==p[y].codigo){
+
+printf("\nProduto: %s Estoque:
+
+%i",p[y].descricao,p[y].estoque);
+
+printf("\nDigite a quantidade ou o para
+
+sair: ");
+
+scanf("%i",&qtde);
+
+if(qtde!=0){
+
+}
+
+}
+
+int main(){
+
+setlocale(LC_ALL, "Portuguese");
+
+int op;
+
+while(op!=5){
+
+menu();
+
+scanf("%i",&op);
+
+switch(op){
+
+case 1:
+
+cadastro();
+
+break;
+
+case 2:
+
+break;
+
+case 3:
+
+relatorio();
+
+break;
+
+case 4:
+
+gerenciamento();
+
+break;
+
+case 5:
+
+printf("\nSaindo da aplicação\n");
+
+system("pause");
+
+default:
+
+printf("\nOpção inválida!\n");
+
+}
+
+}
